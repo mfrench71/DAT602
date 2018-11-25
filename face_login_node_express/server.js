ca5c3425b5e786ca6c39924ba139a6023b28f549
Matthew <mfrench71@googlemail.com>
2018-11-25 13:32:50 +0000

    Remove unused npm modules

--- face_login_node_express/server.js
+++ face_login_node_express/server.js
@@ -1,23 +1,21 @@
 // require modules
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
-var twitter  = require('twit');
-var hbs 	 = require('hbs');
-
-var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser   = require('body-parser');
-var session      = require('express-session');
+var express         = require('express');
+var app             = express();
+var port            = 8080;
+var mongoose        = require('mongoose');
+var passport        = require('passport');
+var flash           = require('connect-flash');
+var twitter         = require('twit');
+var hbs             = require('hbs');
+var morgan          = require('morgan');
+var cookieParser    = require('cookie-parser');
+var bodyParser      = require('body-parser');
+var session         = require('express-session');
 
 // load config variables
-var configDB = require('./config/database');
-var config = require('./config/auth');
-
-var twitter = new twitter(config);
+var configDB        = require('./config/database');
+var config          = require('./config/auth');
+var twitter         = new twitter(config);
 
 // connect to database
 // mongoose.connect(configDB.url); (deprecated)
@@ -32,7 +30,7 @@ app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public')) // serve static files
+app.use(express.static('public')); // serve static files
 
 // handlebars helper (columns)
 hbs.registerHelper('grouped_each', function(every, context, options) {
